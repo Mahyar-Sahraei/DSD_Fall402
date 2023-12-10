ab6510636b8097415a51559ff70bb2e3a07dded0
Mahyar <sahraei19@gmail.com>
2023-12-10 12:30:43 +0330

    Restructured the code + Cleanups

--- UART/uart.vhd
+++ UART/uart.vhd
@@ -18,7 +18,7 @@ ENTITY uart IS
 END uart;
 
 ARCHITECTURE behavioural OF uart IS
-	TYPE state_t is (S0, S1, S2, S3, S4, S5, S6, S7, S8);
+	TYPE state_t is (SS, S0, S1, S2, S3, S4, S5, S6, S7, S8, SD);
 	
 	SIGNAL cur_recv_state, nxt_recv_state: state_t := S0;
 	SIGNAL cur_trsm_state, nxt_trsm_state: state_t := S0;
@@ -44,132 +44,150 @@ BEGIN
 		END IF;
 	END PROCESS trsm_sequential;
 	
-	recv_cobmbinational: PROCESS(cur_recv_state, clk, rx)
+	recv_cobmbinational: PROCESS(cur_recv_state, data_out_reg, clk, rx)
 	BEGIN
+		nxt_recv_state <= cur_recv_state;
+		strobe <= '0';
+
 		CASE cur_recv_state IS
 		WHEN S0 =>
-		  IF rx = '0' THEN
-			nxt_recv_state <= S1;
-		  END IF;
-		  strobe <= '0';
+		  	IF rx = '0' THEN
+				nxt_recv_state <= S1;
+		  	END IF;
 		  
 		WHEN S1 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S3;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S3;
+		  	END IF;
 		  
 		WHEN S2 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S3;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S3;
+		  	END IF;
 		  
 		WHEN S3 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S4;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S4;
+		  	END IF;
 		  
 		WHEN S4 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S5;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S5;
+		  	END IF;
 		  
 		WHEN S5 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S6;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S6;
+		  	END IF;
 		  
 		WHEN S6 =>
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S7;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S7;
+		  	END IF;
 		  
 		WHEN S7 =>
-		  IF rising_edge(clk) THEN
-			data_out <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S8;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				data_out <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= S8;
+		  	END IF;
 		  
-		WHEN OTHERS => -- S8
-		  IF rising_edge(clk) THEN
-			data_out_reg <= rx & data_out_reg(6 DOWNTO 0);
-			nxt_recv_state <= S0;
-			data_out <= data_out_reg;
+		WHEN S8 =>
+		  	IF rising_edge(clk) THEN
+				data_out <= rx & data_out_reg(6 DOWNTO 0);
+				nxt_recv_state <= SD;
+				strobe <= '1';
+		  	END IF;
+
+		WHEN OTHERS => -- SD
 			strobe <= '1';
-		  END IF;
+			IF rising_edge(clk) THEN
+				nxt_recv_state <= S0;
+				data_out <= "00000000";
+				strobe <= '0';
+			END IF;
+
 		END CASE;
+
 	END PROCESS recv_cobmbinational;
 	
-	trsm_cobmbinational: PROCESS(cur_trsm_state, data_in)
+	trsm_cobmbinational: PROCESS(cur_trsm_state, data_in, clk, start)
 	BEGIN
+		nxt_trsm_state <= cur_trsm_state;
+		tx <= '1';
+
 		CASE cur_trsm_state IS
 		WHEN S0 =>
-		  IF start = '1' AND rising_edge(clk) THEN
-			nxt_trsm_state <= S1;
-		  END IF;
+			IF start = '1' AND rising_edge(clk) THEN
+				nxt_trsm_state <= SS;
+			END IF;
+
+		WHEN SS =>
+			IF rising_edge(clk) THEN
+				tx <= '0';
+				nxt_trsm_state <= S1;
+			END IF;
 		  
 		WHEN S1 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(0);
-			nxt_trsm_state <= S3;
-		  END IF;
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(0);
+				nxt_trsm_state <= S3;
+		  	END IF;
 		  
 		WHEN S2 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(1);
-			nxt_trsm_state <= S3;
-		  END IF;
-		  strobe <= '0';
+			IF rising_edge(clk) THEN
+				tx <= data_in(1);
+				nxt_trsm_state <= S3;
+			END IF;
 		  
 		WHEN S3 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(2);
-			nxt_trsm_state <= S4;
-		  END IF;
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(2);
+				nxt_trsm_state <= S4;
+		  	END IF;
 		  
 		WHEN S4 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(3);
-			nxt_trsm_state <= S5;
-		  END IF;
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(3);
+				nxt_trsm_state <= S5;
+		  	END IF;
 		  
 		WHEN S5 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(4);
-			nxt_trsm_state <= S6;
-		  END IF;
-		  strobe <= '0';
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(4);
+				nxt_trsm_state <= S6;
+		  	END IF;
 		  
 		WHEN S6 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(5);
-			nxt_trsm_state <= S7;
-		  END IF;
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(5);
+				nxt_trsm_state <= S7;
+		  	END IF;
 		  
 		WHEN S7 =>
-		  IF rising_edge(clk) THEN
-			tx <= data_in(6);
-			nxt_trsm_state <= S8;
-		  END IF;
-		  
-		WHEN OTHERS => -- S8
-		  IF rising_edge(clk) THEN
-			tx <= data_in(7);
-			nxt_trsm_state <= S0;
-		  END IF;
+		  	IF rising_edge(clk) THEN
+				tx <= data_in(6);
+				nxt_trsm_state <= S8;
+		  	END IF;
+			  
+		WHEN S8 =>
+			IF rising_edge(clk) THEN
+				tx <= data_in(7);
+				nxt_trsm_state <= SD;
+			END IF;
+
+		WHEN OTHERS => -- SD
+			IF rising_edge(clk) THEN
+				nxt_trsm_state <= S0;
+			END IF;
+
 		END CASE;
+
 	END PROCESS trsm_cobmbinational;
 
 END behavioural;
